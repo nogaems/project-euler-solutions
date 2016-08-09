4cadcd360f740d970e792d0ea639d363c5a5b2ac
nogaems <fakemailn1@mail.ru>
2016-08-09 09:17:32 -0400

    Create bernoulli.py

---
+++ utils/bernoulli.py
@@ -0,0 +1,19 @@
+from permutation import get_count
+from fractions import Fraction
+
+def get_numbers(N):
+    """
+    Returns a list of the Bernoulli numbers up to N inclusive.
+
+    """
+    N = int(N)
+    B = [Fraction(0)] * (N+1)
+    B[0] = Fraction(1)
+    B[1] = Fraction(-1, 2)
+    for n in xrange(2, N+1, 2):
+        coef = Fraction(-1, n + 1)
+        adder = Fraction(0)
+        for k in xrange(1, n + 1):
+            adder = adder + get_count(n + 1, k + 1) * B[n-k]
+        B[n] = coef * adder
+    return B
