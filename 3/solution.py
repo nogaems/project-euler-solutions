aed17e1b5ea6fd48a23ea6becee0f985a9c772f7
nogaems <fakemailn1@mail.ru>
2016-08-09 03:32:11 -0400

    Update solution.py of #3 problem

--- 3/solution.py
+++ 3/solution.py
@@ -1,16 +1,4 @@
-from utils.soe2 import get_primes
-from math import sqrt, ceil
+from utils.factorization import get_max_factor
 
 target = 600851475143  # constant in conditions of problem
-search = 1  # initial value
-top_border = int(ceil(sqrt(600851475143)))  # the calculation of the upper limit of the required sequence of prime numbers
-primes = get_primes(top_border)
-for n in primes:
-    while True:  # repeating the division as long as possible
-        if target % n == 0: 
-            target = target / n
-            search = n
-        else:
-            break
-
-print(search)
+print(get_max_factor(target))
