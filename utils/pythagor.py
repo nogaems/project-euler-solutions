e4d76fb79a2eb3d0ae9c6b9b7750ddc63990b170
nogaems <fakemailn1@mail.ru>
2016-08-14 10:05:21 -0400

    Create pythagor.py

---
+++ utils/pythagor.py
@@ -0,0 +1,34 @@
+import math
+
+def get_triples(c_max=5):    
+    """
+    a^2 + b^2 = c^2
+    a < b < c
+
+    e.g 3^2 + 4^2 = 5^2
+    """
+    
+    if not isinstance(c_max, int):
+        raise TypeError('\'c_max\' must be \'int\'')
+    elif c_max < 5:
+        raise ValueError('\'c_max\' must be greater than \'4\'')
+    
+    f1 = lambda m, n: m ** 2 - n ** 2
+    f2 = lambda m, n: 2 * m * n
+    f3 = lambda m, n: m ** 2 + n ** 2
+
+    triples = []
+    m = 2
+    
+    while True:
+        if m > int(math.sqrt(c_max - 1)):
+            return triples
+        for n in range(1, m):
+            if n == 1 or m % n != 0:
+                a, b, c = sorted([f1(m, n), f2(m, n), f3(m, n)])
+                if c > c_max:
+                    break                    
+                if c % b != 0 and b % a != 0 and c % a != 0 and [a, b, c] not in triples:
+                    triples.append([a, b, c])
+        m += 1
+
